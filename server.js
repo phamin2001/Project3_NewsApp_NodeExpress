2b32af9e2e96db1eb265881313ce26761e510056
phamin2001 <amin.pahlavani@gmail.com>
2019-03-04 22:37:51 -0700

    server

--- server.js
+++ server.js
@@ -3,9 +3,21 @@ const app           = express();
 const bodyParser    = require('body-parser');
 const cors          = require('cors');
 const session       = require('express-session');
+const morgan        = require('morgan');
 
+require('dotenv').config();
 require('./db/db');
 
+const MongoDBStore = require('connect-mongodb-session')(session);
+const store = new MongoDBStore({
+    uri: process.env.MONGODB_URI,
+    collection: 'mySessions'
+});
+
+store.on('error', function(error) {
+    console.log(error);
+  });
+  
 app.use(session({
     secret: 'keyboard cat',
     resave: false,
@@ -21,6 +33,7 @@ const corsOption = {
     optionsSuccessStatus: 200
 }
 app.use(cors(corsOptions));
+app.use(morgan('short'));
 
 const newsController    = require('./controllers/newsController');
 const authController    = require('./controllers/authController');
